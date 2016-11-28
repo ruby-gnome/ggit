7f56b09609f8dd5614340cfb10e9aa558191c4c8
cedlemo <cedlemo@gmx.com>
2016-11-28 16:04:38 +0100

    Add tests for Ggit::Repository#clone

--- test/test-repository.rb
+++ test/test-repository.rb
@@ -16,8 +16,36 @@ class TestRepository < Test::Unit::TestCase
     assert(File.exist?(@path + "/bare_repo/test/.git/config"))
   end
 
+  def test_repository_clone
+    url = "https://github.com/ruby-gnome2/ggit.git"
+    destination = Gio::File.path("#{@path}/ggit-clone")
+    Ggit::Repository.clone(url, destination)
+    assert(File.exist?(@path + "/ggit-clone/.git/config"))
+  end
+
+  def test_repository_bare_clone
+    options = Ggit::CloneOptions.new
+    options.set_is_bare(true)
+    url = "https://github.com/ruby-gnome2/ggit.git"
+    destination = Gio::File.path("#{@path}/ggit-clone-bare")
+    Ggit::Repository.clone(url, destination, options)
+    assert(File.exist?(@path + "/ggit-clone-bare/config"))
+  end
+
+  def test_repository_checkout_clone
+    options = Ggit::CloneOptions.new
+    options.set_checkout_branch("master")
+    url = "https://github.com/ruby-gnome2/ggit.git"
+    destination = Gio::File.path("#{@path}/ggit-clone-checkout")
+    Ggit::Repository.clone(url, destination, options)
+    assert(File.exist?(@path + "/ggit-clone-checkout/.git/config"))
+  end
+
   def cleanup
     FileUtils.rm_rf("#{@path}/non_bare_repo")
     FileUtils.rm_rf("#{@path}/bare_repo")
+    FileUtils.rm_rf("#{@path}/ggit-clone")
+    FileUtils.rm_rf("#{@path}/ggit-clone-bare")
+    FileUtils.rm_rf("#{@path}/ggit-clone-checkout")
   end
 end
